f1555f424cef206fc0b66d816c1ab5f30f0d107e
Frederik Petersen <43568735+fredpetersen@users.noreply.github.com>
2022-09-02 13:20:29 +0200

    Calendar leap year (#2)
    * Test and implement divisible by 4
    
    * Test and implement divisble by 100 rule
    
    * Test and implement divisible by 400 rule

---
+++ TestProject.Tests/CalendarTest.cs
@@ -0,0 +1,44 @@
+namespace TestProject.Tests;
+
+public class CalendarTest
+{
+    [Fact]
+    public void IsLeapYear_given_2004_returns_true()
+    {
+        var sut = new Calendar();
+
+        var result = sut.IsLeapYear(2004);
+
+        result.Should().Be(true);
+    }
+
+    [Fact]
+    public void IsLeapYear_given_2001_returns_false()
+    {
+        var sut = new Calendar();
+
+        var result = sut.IsLeapYear(2001);
+        
+        result.Should().Be(false);
+    }
+
+    [Fact]
+    public void IsLeapYear_given_1800_returns_false()
+    {
+        var sut = new Calendar();
+
+        var result = sut.IsLeapYear(1800);
+
+        result.Should().Be(false);
+    }
+
+    [Fact]
+    public void IsLeapYear_given_2000_returns_true()
+    {
+        var sut = new Calendar();
+
+        var result = sut.IsLeapYear(2000);
+
+        result.Should().Be(true);
+    }
+}
---
+++ TestProject/Calendar.cs
@@ -0,0 +1,9 @@
+﻿namespace TestProject;
+
+public class Calendar{
+    public bool IsLeapYear(int year){
+        if(year % 400 == 0) return true;
+        if(year % 100 == 0) return false;
+        return year % 4 == 0;
+    }
+}
